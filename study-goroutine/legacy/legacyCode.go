166f645ddd034faad8ba43caf85d8308d6e4e5c9
penta-kihyun <71832733+kihyun1998@users.noreply.github.com>
2023-08-17 13:06:42 +0900

    go routine

---
+++ study-goroutine/legacy/legacyCode.go
@@ -0,0 +1,30 @@
+package legacy
+
+// 기본 ==========================
+// func hello(n int) {
+// 	r := rand.Intn(100)
+// 	time.Sleep(time.Duration(r))
+// 	fmt.Println(n)
+// }
+
+// func main() {
+// 	for i := 0; i < 100; i++ {
+// 		go hello(i)
+// 	}
+// 	fmt.Scanln()
+// }
+
+// 멀티코어 ========================
+// func main() {
+// 	runtime.GOMAXPROCS(runtime.NumCPU()) //멀티코어 사용
+// 	fmt.Println(runtime.GOMAXPROCS(0))
+
+// 	s := "hello"
+
+// 	for i := 0; i < 100; i++ {
+// 		go func(n int) {
+// 			fmt.Println(s, n)
+// 		}(i) // << 얘가 함수 인자값
+// 	}
+// 	fmt.Scanln()
+// }
--- study-goroutine/main.go
+++ study-goroutine/main.go
@@ -1,39 +1,21 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"math/rand"
-// 	"time"
-// )
-
-// func hello(n int) {
-// 	r := rand.Intn(100)
-// 	time.Sleep(time.Duration(r))
-// 	fmt.Println(n)
-// }
-
-// func main() {
-// 	for i := 0; i < 100; i++ {
-// 		go hello(i)
-// 	}
-// 	fmt.Scanln()
-// }
-
 import (
 	"fmt"
 	"runtime"
 )
 
+// 클로저를 go 루틴으로 실행
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	fmt.Println(runtime.GOMAXPROCS(0))
+	runtime.GOMAXPROCS(1)
 
 	s := "hello"
 
 	for i := 0; i < 100; i++ {
-		go func(n int) {
-			fmt.Println(s, n)
-		}(i) // << 얘가 함수 인자값
+		go func() {
+			fmt.Println(s, i)
+
+		}()
 	}
 	fmt.Scanln()
 }
